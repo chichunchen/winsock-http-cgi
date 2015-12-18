9b426ffe2ed1b805cf1263b4909e949e49282d0c
Chi-Chun, Chen <chichunchen844@gmail.com>
2015-12-18 16:29:54 +0800

    Update layout by the query string.

--- NP_hw3.cpp
+++ NP_hw3.cpp
@@ -8,8 +8,10 @@ using namespace std;
 
 /* Define Macro */
 
+#define DEBUG				0
 #define SERVER_PORT			7799
 #define WM_SOCKET_NOTIFY	(WM_USER + 1)
+#define WM_SERVER_RESPONSE	7777
 #define RECV_BUF_SIZE		10000
 
 /* Function Prototypes */
@@ -21,8 +23,9 @@ void write_html_header(int connfd);
 int parse_query_string(char *qs);
 void html_init(int connfd);
 void html_end(int connfd);
-void server_hanlder();
-void setup_connection(int index);
+void server_hanlder(HWND hwndEdit, HWND hwnd, SOCKET sock);
+void setup_connection(HWND hwndEdit, HWND hwnd, int index);
+void serve_connection();
 
 //=================================================================
 //	Global Variables
@@ -32,6 +35,110 @@ char buf[RECV_BUF_SIZE];
 int  content_length;
 char request_filename[100];
 Request requests[5];
+
+void WriteToSock(SOCKET sock, char *s) {
+
+	int r;
+
+	r = send(sock, s, strlen(s), 0);
+	if (r == SOCKET_ERROR) {
+		OutputDebugString("=== Error: socket send ===\r\n");
+		WSACleanup();
+		return;
+	}
+
+}
+
+// HTML Client
+char *replace_str(const char *str, const char *old, const char *new_str) {
+
+	char *ret, *r;
+	const char *p, *q;
+	int oldlen = strlen(old);
+	int count, retlen, newlen = strlen(new_str);
+	int samesize = (oldlen == newlen), l;
+
+	if (!samesize) {
+		for (count = 0, p = str; (q = strstr(p, old)) != NULL; p = q + oldlen)
+			count++;
+		retlen = p - str + strlen(p) + count * (newlen - oldlen);
+	}
+	else
+		retlen = strlen(str);
+
+	if ((ret = (char *)malloc(retlen + 1)) == NULL)
+		return NULL;
+
+	r = ret, p = str;
+	while (1) {
+		if (!samesize && !count--)
+			break;
+		if ((q = strstr(p, old)) == NULL)
+			break;
+		l = q - p;
+		memcpy(r, p, l);
+		r += l;
+		memcpy(r, new_str, newlen);
+		r += newlen;
+		p = q + oldlen;
+	}
+	strcpy(r, p);
+
+	return ret;
+
+}
+
+char *wrap_html(char *s) {
+	fprintf(stderr, "wrapping:\n%s\n", s);
+	char *r;
+	r = s;
+	r = replace_str(r, "&", "&amp;");
+	r = replace_str(r, "\"", "&quot;");
+	r = replace_str(r, "<", "&lt;");
+	r = replace_str(r, ">", "&gt;");
+	r = replace_str(r, "\r\n", "\n");
+	r = replace_str(r, "\n", "<br />");
+	return r;
+}
+
+void write_head_at(SOCKET sock, int num, char *_content) {
+	char content[RECV_BUF_SIZE];
+	sprintf(content, "<script>document.all['res_tr_head'].innerHTML += \"<td>%s</td>\";</script>", _content);
+	WriteToSock(sock, content);
+}
+
+void write_content_at(SOCKET sock, int num, char *_content, int bold) {
+	char content[RECV_BUF_SIZE];
+	if (bold)
+		sprintf(content, "<script>document.all('c-%d').innerHTML += \"<b>%s</b>\";</script>", num, _content);
+	else
+		sprintf(content, "<script>document.all('c-%d').innerHTML += \"%s\";</script>", num, _content);
+	WriteToSock(sock, content);
+}
+
+void write_content_init(SOCKET sock, int num) {
+	char content[RECV_BUF_SIZE];
+	sprintf(content, "<script>\
+					 		    document.all('res_tr_content').innerHTML += \"\
+														<td id='c-%d'></td>\";</script>", num);
+	WriteToSock(sock, content);
+}
+
+void serve_req_at(SOCKET sock, int num) {
+
+	write_head_at(sock, num, requests[num].ip);
+	write_content_init(sock, num);
+
+}
+
+void serve_req(SOCKET sock) {
+	int i;
+	for (i = 0; i < REQUEST_MAX_NUM; i++) {
+		Request r = requests[i];
+		if (!(r.ip && r.port && r.filename))   continue;
+		serve_req_at(sock, i);
+	}
+}
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 	LPSTR lpCmdLine, int nCmdShow)
@@ -45,7 +152,7 @@ BOOL CALLBACK MainDlgProc(HWND hwnd, UINT Message, WPARAM wParam, LPARAM lParam)
 	WSADATA wsaData;
 
 	static HWND hwndEdit;
-	static SOCKET msock, ssock;
+	static SOCKET msock, ssock, csock;
 	static struct sockaddr_in sa;
 
 	int err;
@@ -126,7 +233,6 @@ BOOL CALLBACK MainDlgProc(HWND hwnd, UINT Message, WPARAM wParam, LPARAM lParam)
 					EditPrintf(hwndEdit, TEXT("=== Accept one new client(%d), List size:%d ===\r\n"), ssock, Socks.size());
 					break;
 				case FD_READ:
-				//Write your code for read event here.
 					ZeroMemory(buf, RECV_BUF_SIZE);
 					content_length = recv(ssock, buf, RECV_BUF_SIZE, 0);
 					EditPrintf(hwndEdit, TEXT("=== length: (%d) ===\r\n"), content_length);
@@ -152,19 +258,19 @@ BOOL CALLBACK MainDlgProc(HWND hwnd, UINT Message, WPARAM wParam, LPARAM lParam)
 
 					parse_query_string(query_str);
 
-					break;
-				case FD_WRITE:
-				//Write your code for write event here
 					FILE *file_ptr;		/* for open the request html file */
 
 					/* start our cgi client */
 					if (strcmp(request_filename, "hw3.cgi") == 0) {
 						write_html_header(ssock);
-						
+
 						html_init(ssock);
 
-						/* start connection */
-						server_hanlder();
+						/* update the layout correspond to requests */
+						serve_req(ssock);
+
+						/* start server connections */
+						//server_hanlder(hwndEdit, hwnd, ssock);
 
 						html_end(ssock);
 
@@ -188,12 +294,13 @@ BOOL CALLBACK MainDlgProc(HWND hwnd, UINT Message, WPARAM wParam, LPARAM lParam)
 						}
 						//OutputDebugString(buf);
 						send(ssock, buf, strlen(buf), 0);
-						
+
 						/* release resources */
 						fclose(file_ptr);
 						closesocket(ssock);
 					}
-
+					break;
+				case FD_WRITE:
 					break;
 				case FD_CLOSE:
 					break;
@@ -271,20 +378,27 @@ void html_end(int connfd)
 /*---------------------- handle server ------------------------*/
 /*-------------------------------------------------------------*/
 
-void server_hanlder()
+void server_hanlder(HWND hwndEdit, HWND hwnd, SOCKET sock)
 {
 	for (int i = 0; i < REQUEST_MAX_NUM; i++) {
 		Request r = requests[i];
 		if (!(r.ip && r.port && r.filename)) continue;
 
 		/* connect */
-		setup_connection(i);
+		setup_connection(hwndEdit, hwnd, i);
+		OutputDebugString("i\n");
 	}
 	// serve
+	serve_connection();
 }
 
 /* setup connection by given index of requests */
-void setup_connection(int index)
+void setup_connection(HWND hwndEdit, HWND hwnd, int index)
+{
+
+}
+
+void serve_connection()
 {
 
 }
@@ -343,18 +457,18 @@ void parse_key_value(char *token)
 
 int parse_query_string(char *qs)
 {
-	char *token;
+	char *token, *save_ptr;
 
 	if (!qs) return -1;
 
-	token = strtok(qs, "&");
+	token = strtok_s(qs, "&", &save_ptr);
 	//OutputDebugString("token ");
 	//OutputDebugString(token);
 
 	int  i = 0;
 	while (token) {
 		parse_key_value(token);
-		token = strtok(NULL, "&");
+		token = strtok_s(NULL, "&", &save_ptr);
 		//OutputDebugString("token ");
 		//OutputDebugString(token);
 
